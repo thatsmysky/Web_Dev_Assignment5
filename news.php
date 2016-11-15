32e24b1641efdd1d8dce9c0bf1a7aeac7be45432
Lucy Kull <lucy.r.kull@gmail.com>
2016-11-14 18:49:09 -0600

    Updated

--- news.php
+++ news.php
@@ -15,7 +15,17 @@ $heading = "";
 $stories = "";
 
 //make html for the headings and stories
-
+for ($j = 0; $j < $row_count; ++$j) {
+    $row = mysqli_fetch_array($result); //fetch the next row
+    $heading.=display_heading($row);
+}
+   
 
 mysqli_close($connection);
+
+function display_heading($row) {
+    $heading = '<div class ="news_item" >' . $row["Heading"] . '</div>';
+    $heading .= '<img class = "news_img" src="images/NBA.jpg">';
+    return $heading;
+}
 ?>
