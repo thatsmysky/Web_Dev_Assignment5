01d18cfd1bf5191e98656b8e67d1715130583278
Lucy Kull <lucy.r.kull@gmail.com>
2016-11-16 17:39:43 -0600

    updated news.php
    works

--- news.php
+++ news.php
@@ -22,26 +22,23 @@ for ($j = 0; $j < $row_count; ++$j) {
     $heading.=display_heading($row);
     $stories.=load_detail($row);
 }
-/*
-//Make HTML for all of the news details
-for ($j = 0; $j < $row_count; ++$j) {
-    $row = mysqli_fetch_array($result); //fetch the next row
 
-}*/
   
 mysqli_close($connection);
 
+//Make HTML for marquee
 function display_heading($row) {
     $heading = '<div class ="news_item" data-story-id=' . $row["ID"] . '>' . $row["Heading"] . '</div>';
     $heading .= '<img class = "news_img" src="images/NBA.jpg">';
     return $heading;
 }
 
+//Make HTML for details
 function load_detail($row) {
     $detail = '<div class="detail_hidden" id=' . $row["ID"] . ' >';
     $detail.= '<img class="news_photo" src="data:' .$row["Picture_Type"] . ';base64,' . base64_encode($row["Picture"]) . '">';
     $detail.= '<div class="news_heading">' . $row["Heading"] . '</div>';
-    $detail.= '<div class="date"' . $row["Date"] . '</div>';
+    $detail.= '<div class="date">' . $row["Date"] . '</div>';
     $detail.= '<div class="news_detail">' . $row["Details"] . '</div>';
     $detail.= '</div>';
     return $detail;
