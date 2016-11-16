7083ed385dc761c60e38cf7da6d4095fdd48c902
Lucy Kull <lucy.r.kull@gmail.com>
2016-11-16 17:26:39 -0600

    Update news.php

--- news.php
+++ news.php
@@ -3,29 +3,48 @@
 
 require_once 'connection.php';
 
-
+//Get from the database all the news
 $query = "SELECT * FROM News";
 $result = mysqli_query($connection,$query);
 
+//If database failed or doesn't exist, throw an error
 if (!$result)
     die("Database access failed: ");
 
+//Variables
 $row_count = mysqli_num_rows($result);
-$heading = "";
-$stories = "";
+$heading = ""; //Heading for marquee - gather HTML from database for headings
+$stories = ""; //HTML for body - all details will be hidden at first
 
-//make html for the headings and stories
+//Make HTML for the headings and stories
 for ($j = 0; $j < $row_count; ++$j) {
     $row = mysqli_fetch_array($result); //fetch the next row
     $heading.=display_heading($row);
+    $stories.=load_detail($row);
 }
-   
+/*
+//Make HTML for all of the news details
+for ($j = 0; $j < $row_count; ++$j) {
+    $row = mysqli_fetch_array($result); //fetch the next row
 
+}*/
+  
 mysqli_close($connection);
 
 function display_heading($row) {
-    $heading = '<div class ="news_item" >' . $row["Heading"] . '</div>';
+    $heading = '<div class ="news_item" data-story-id=' . $row["ID"] . '>' . $row["Heading"] . '</div>';
     $heading .= '<img class = "news_img" src="images/NBA.jpg">';
     return $heading;
+}
+
+function load_detail($row) {
+    $detail = '<div class="detail_hidden" id=' . $row["ID"] . ' >';
+    $detail.= '<img class="news_photo" src="data:' .$row["Picture_Type"] . ';base64,' . base64_encode($row["Picture"]) . '">';
+    $detail.= '<div class="news_heading">' . $row["Heading"] . '</div>';
+    $detail.= '<div class="date"' . $row["Date"] . '</div>';
+    $detail.= '<div class="news_detail">' . $row["Details"] . '</div>';
+    $detail.= '</div>';
+    return $detail;
+    
 }
 ?>
